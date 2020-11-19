c8452b4e60171266bb084230be340cea59b49235
Nik Sofianos <nsofianos@hotmail.com>
2020-11-19 00:45:05 +0000

    setup page with basic styling, with form for posting tweet

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,56 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+  const createTweetElement = (tweet) => {
+    const $tweet = $(`
+    <article class="tweet">
+    <header class="tweet-header">
+      <h2>${tweet.user.name}</h2>
+      <h3 class="my-tag">${tweet.user.handle}</h3>
+    </header>
+    <div>
+      <p>${tweet.content.text}</p>
+    </div>
+    <hr>
+    <footer class="tweet-footer">
+      <p>10 days ago</p>
+      <p class="icons">icons</p>
+    </footer>
+  </article>`);
+    return $tweet;
+  };
+  
+  const renderTweets = (tweets) => {
+    for (const tweet of tweets) {
+      $("#tweets-container").append(createTweetElement(tweet));
+    }
+  }
+  
+  $(document).ready(function() {
+    renderTweets(tweetData);
+  });
---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  // --- our code goes here ---
+  $("#tweet-text").on('input', function() {
+    //console.log(($(".counter").val() - $("#tweet-text").val().length));
+    console.log($(".counter").val());
+    const newval = (140 - $("#tweet-text").val().length);
+    //console.log(newval);
+    $(".counter").val(newval);
+  });
+});
